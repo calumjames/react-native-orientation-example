80e2c964268d3249ae2a76c13eb5913ec4830223
Calum James <calum.james@oracle.com>
2019-04-25 23:50:24 +0100

    A simple example of the text changing when the orientation changes

---
+++ App.js
@@ -0,0 +1,54 @@
+
+import React, { PureComponent } from "react";
+import { StyleSheet, Dimensions, Text, View } from "react-native";
+import { AppLoading, Font, Icon, ScreenOrientation } from "expo";
+
+class App extends PureComponent {
+  state = {
+    orientation: ScreenOrientation.Orientation.LANDSCAPE
+  };
+
+  layoutHandler = ({ window: { width, height } }) => {
+    let orientation;
+    if (width > height) {
+      orientation = ScreenOrientation.Orientation.LANDSCAPE;
+    } else {
+      orientation = ScreenOrientation.Orientation.PORTRAIT;
+    }
+    ScreenOrientation.allowAsync(orientation);
+    this.setState({ orientation });
+  };
+
+  componentDidMount() {
+    Dimensions.addEventListener("change", this.layoutHandler);
+  }
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.layoutHandler);
+  }
+
+  render = () => {
+    const { orientation, loadingIsComplete } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{orientation}</Text>
+      </View>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 40,
+    color: "#ccc",
+    textTransform: "uppercase"
+  }
+});
+
+export default App;
